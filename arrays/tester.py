449fe83bac0b0de6104d166216334b191bacd764
CsugnaBro <csunga16@gmail.com>
2022-01-25 21:30:32 +0100

    updated for the 2. task

--- arrays/tester.py
+++ arrays/tester.py
@@ -3,7 +3,7 @@ from logger import logger_init
 class Tester:
     def tester(self,func,tests):
         for count, test in enumerate(tests):
-            if func(test[0], test[1]) == test[2]:
+            if func(test[0]) == test[1]:
                 logger.info(f"{count}. Test Passed")
             else:
                 logger.error("Failed: {} insted of: {}".format(func(test[0], test[1]), test[2]))
