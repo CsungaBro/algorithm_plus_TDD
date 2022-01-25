5362e874a9c06f6f6ddfe80eaa81fac389c2efc2
CsugnaBro <csunga16@gmail.com>
2022-01-25 17:49:42 +0100

    implemented two sum for arranged lists

---
+++ arrays/tester.py
@@ -0,0 +1,11 @@
+from logger import logger_init
+
+class Tester:
+    def tester(self,func,tests):
+        for count, test in enumerate(tests):
+            if func(test[0], test[1]) == test[2]:
+                logger.info(f"{count}. Test Passed")
+            else:
+                logger.error("Failed: {} insted of: {}".format(func(test[0], test[1]), test[2]))
+
+logger = logger_init()
--- arrays/two-sum.py
+++ arrays/two-sum.py
@@ -1,31 +1,52 @@
 from logger import logger_init
-
-class Solution:
-    def __init__(self) -> None:
-        self.tester()
-
+from tester import Tester
+class Solution(Tester):
     def twoSum(self, nums, target):
-        return [4,5]
-
-    def tester(self):
-        tests = [
-            [
-                [1,2,5,6,8,10,11,15,19],
-                18,
-                [4,5]
-            ],
-            [
-                [2,7,11,15],
-                9,
-                [0,1]
-            ]
-        ]
-        for test in tests:
-            if self.twoSum(test[0], test[1]) == test[2]:
-                logger.info("Passed")
+        cont = [-1, -1]
+        for count, num in enumerate(nums):
+            new_nums = nums[count + 1:]
+            new_target = target - num
+            other_num = self.binary_search(new_nums, 0, len(new_nums)-1, new_target)
+            if other_num == -1:
+                continue
             else:
-                logger.error("Failed: {} insted of: {}".format(self.twoSum(test[0], test[1]), test[2]))
+                cont = [count, count + other_num + 1] 
+                break
+        return cont
+
+    def binary_search(self, nums, low, high, target):
+        if high >= low:
+            mid = int((high + low) // 2)
+            if nums[mid] == target:
+                return mid
+
+            elif nums[mid] > target:
+                return self.binary_search(nums, low, mid - 1, target)
+
+            elif nums[mid] < target:
+                return self.binary_search(nums, mid + 1, high, target)
+
+        else:
+            return -1
 
 
-logger = logger_init()
+
+tests = [
+    [
+        [1,2,5,6,8,10,11,15,19],18,[4,5]],
+    [
+        [2,7,11,15],9,[0,1]
+    ],
+    [
+        [3,2,4],6,[1,2]
+    ],
+    [
+        [3,3],6,[0,1]
+    ],
+    [
+        [0,4,3,0],0,[0,3]        
+    ]
+]
+
 S = Solution()
+S.tester(S.twoSum, tests)
