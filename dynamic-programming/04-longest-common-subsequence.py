8a953faa0675bd788211908d573df3a046542584
CsugnaBro <csunga16@gmail.com>
2022-02-01 13:53:45 +0100

    04-done

--- dynamic-programming/04-longest-common-subsequence.py
+++ dynamic-programming/04-longest-common-subsequence.py
@@ -7,35 +7,15 @@ class Solution(Tester):
         self.num = {0:0}
 
     def maxProfit(self, text1, text2):
-        if len(text1) > len(text2):
-            text_short = text2
-            text_long = text1
-        else:
-            text_short = text1
-            text_long = text2
-        len_common = self.cmonBro(text_short, text_long)
-        len_common = max(len_common, self.cmonBro(text_long, text_short))
-        return len_common
-
-    def cmonBro(self, text_short, text_long):
-        common_text =""
-        pointer_short = 0
-        pointer_long = 0
-        while pointer_long < len(text_long):
-            if text_short[pointer_short] == text_long[pointer_long]:
-                common_text += text_short[pointer_short]
-                text_short = text_short[pointer_short+1:]
-                text_long = text_long[pointer_long+1:]
-                pointer_long, pointer_short = 0, 0
-                if len(text_short) == 0:
-                    return len(common_text)
-            else:
-                if pointer_short < len(text_short)-1:
-                    pointer_short += 1
+        dp = [[0 for j in range(len(text2)+1)] for i in range(len(text1)+1)]
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i+1][j+1]+1
                 else:
-                    pointer_short = 0
-                    pointer_long += 1    
-        return len(common_text)
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        print(dp)
+        return dp[0][0]
             
 
 
