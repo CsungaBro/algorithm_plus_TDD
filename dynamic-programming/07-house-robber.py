8572a707a7a91beb7060003a36f5e8ecfdf831be
CsugnaBro <csunga16@gmail.com>
2022-02-02 09:57:44 +0100

    07-done

--- dynamic-programming/07-house-robber.py
+++ dynamic-programming/07-house-robber.py
@@ -7,9 +7,18 @@ class Solution(Tester):
         self.num = {0:0}
 
     def maxProfit(self, nums):
-        pass
+        dp = [0 for i in range(len(nums)+4)]
+        for i in range(len(nums)-1,-1,-1):
+            dp[i+1] = max(dp[i+3],dp[i+4],dp[i+1])
+            dp[i+1] += nums[i]
+        dp[0] = max(dp[2],dp[3])           
+        return max(dp[0],dp[1]) 
+
 
 tests = [
+    [
+      [1,2], 2  
+    ],
     [
         [1,2,3,1], 4
     ],
