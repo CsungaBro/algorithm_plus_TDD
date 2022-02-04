a6461c7242da719e7047d4a7fba18ee14cfcbc30
CsugnaBro <csunga16@gmail.com>
2022-02-04 11:41:31 +0100

    03- almost

--- graph/03-pacific-atlantic-water-flow.py
+++ graph/03-pacific-atlantic-water-flow.py
@@ -16,43 +16,55 @@ class Node:
         
 class Solution(Tester):    
     def maxProfit(self, heights):
-        def plotter():
-            plt.figure()
-            to_plot = np.array(heights)
-            print(to_plot)
-            plt.imshow(to_plot)
-            plt.colorbar()
-            plt.show()  
-        # plotter()
         
-        nodes = {}
+        pacific_ocean = [[0 for j in range(len(heights[0]))] for i in range(len(heights))]
+        atlantic_ocean = [[0 for j in range(len(heights[0]))] for i in range(len(heights))]
         
-        heights_with_ocean = [[0 for j in range(len(heights[0])+2)] for i in range(len(heights)+2)]
-        for i in range(len(heights)+1):
-            for j in range(len(heights[0])+1):
-                if i > 0 and i < len(heights)+1:
-                    if j > 0 and j < len(heights[0])+1:
-                        heights_with_ocean[i][j] = heights[i-1][j-1]
-                        nodes[str(i)+str(j)] = Node([i,j],heights[i-1][j-1])
-                    else:
-                        nodes[str(i)+str(j)] = Node([i,j],0)
-                else:
-                    nodes[str(i)+str(j)] = Node([i,j],0)
-        print(np.array(heights_with_ocean))
-        
-        def neighbour_cheker(x,y):
-            x = int(x)
-            y = int(y)
-            for i in range(-1,2,2):
-                for j in range(-1,2,2):
-                    if nodes[str(x+i)+str(y+j)].height <= nodes[str(x)+str(y)].height:
-                        nodes[str(x)+str(y)].childs.append(nodes[str(x+i)+str(y+j)])
-                    else:
-                        nodes[str(x)+str(y)].parents.append(nodes[str(x+i)+str(y+j)])
+        for i, row in enumerate(pacific_ocean):
+            for j, cord in enumerate(row):
+                if i == 0 or j == 0:
+                    pacific_ocean[i][j] = 1
+                    
+        for i, row in enumerate(atlantic_ocean):
+            for j, cord in enumerate(row):
+                if i == len(atlantic_ocean)-1 or j == len(row)-1:
+                    atlantic_ocean[i][j] = 1
+
+        for i in range(1,len(pacific_ocean)):
+            for j in range(1,len(pacific_ocean[0])):
+                left_height = heights[i][j-1]
+                left_ocean = pacific_ocean[i][j-1]
+                up_height = heights[i-1][j]
+                up_ocean = pacific_ocean[i-1][j]
+                curr_height = heights[i][j]
+                if left_height <= curr_height and left_ocean:
+                    pacific_ocean[i][j] = 1
+                elif up_height <= curr_height and up_ocean:
+                        pacific_ocean[i][j] = 1
+
+        for i in range(len(atlantic_ocean)-2,-1,-1):
+            for j in range(len(atlantic_ocean[0])-2,-1,-1):
+                right_height = heights[i][j+1]
+                right_ocean = atlantic_ocean[i][j+1]
+                
+                down_height = heights[i+1][j]
+                down_ocean = atlantic_ocean[i+1][j]
+                
+                curr_height = heights[i][j]
+                if right_height <= curr_height and right_ocean:
+                    atlantic_ocean[i][j] = 1
+                elif down_height <= curr_height and down_ocean:
+                        atlantic_ocean[i][j] = 1
                         
-        for node in nodes:
-            if not "0" in node:
-                neighbour_cheker(node[0], node[1])
+        both_ocean_list = []
+        for i, row in enumerate(atlantic_ocean):
+            for j, cord in enumerate(row):
+                if pacific_ocean[i][j] == 1 and atlantic_ocean[i][j] == 1:
+                    both_ocean_list.append([i,j])          
+          
+        return both_ocean_list
+        
+
 tests = [
     [
         [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]], [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
